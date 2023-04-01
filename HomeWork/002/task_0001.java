9cb89ff226af6450ba4f6b84cc48ebba0e646c38
Andrey Slobodchikov <andslob@inbox.ru>
2023-04-01 19:55:42 +0500

    1 готово

--- HomeWork/002/task_0001.java
+++ HomeWork/002/task_0001.java
@@ -1,24 +1,37 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 public class task_0001 {
     public static void main(String[] args) {
+        String text = "0001.txt";
+        File task = new File(text);
         int [] array = {13, 28, 30, 29, 28, 22, 3, 1};
- 
+        
         boolean isSorted = false;
         int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < array.length-1; i++) {
-                if(array[i] > array[i+1]){
-                    isSorted = false;
- 
-                    buf = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = buf;
+        try {
+            while(!isSorted) {
+                isSorted = true;
+                for (int i = 0; i < array.length-1; i++) {
+                    if(array[i] > array[i+1]){
+                        isSorted = false;
+    
+                        buf = array[i];
+                        array[i] = array[i+1];
+                        array[i+1] = buf;
+                    }
+                }System.out.println(Arrays.toString(array));
+                FileWriter writer = new FileWriter(text, true);
+                    writer.write(Arrays.toString(array));
+                    writer.write("\n");
+                    writer.close();
+                } 
+                    
                 }
-            }System.out.println(Arrays.toString(array));
-        }
-        
-
-    }
+                catch (Exception e) {
+                    System.out.println("Что-то не то");
+                }
+            
+        } 
 }
--- HomeWork/002/task_002.java
+++ HomeWork/002/task_002.java
@@ -1,6 +1,6 @@
 
-
-
 public class task_002 {
-    
+    public static void main(String[] args) {
+    }
 }
+
