1cdcc57c3542e0721f810d7ab04a27599395eb1b
Andrey Slobodchikov <andslob@inbox.ru>
2023-04-02 17:51:26 +0500

    готово 2 и 3

--- HomeWork/002/task_002.java
+++ HomeWork/002/task_002.java
@@ -1,6 +1,29 @@
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class task_002 {
-    public static void main(String[] args) {
-    }
-}
+    public static void main(String[] args) throws Exception {
+        FileReader f = new FileReader("task_2.txt");
+        Scanner sc = new Scanner(f);
+        String result = sc.nextLine();
+        System.out.println(result);
+        f.close();
+        sc.close();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < result.length(); i++) {
+            if (result.charAt(i) != '"') {
 
+                if (result.charAt(i) == ':') {
+                    res.append(',');
+
+                } else {
+                    res.append(result.charAt(i));
+                }
+            }
+        }
+        String[] arr = res.toString().split(",");
+        System.out.println("Студент " + arr[1] + " получил " + arr[3] + " по предмету " + arr[5] + ".");
+
+    }
+
+}
--- HomeWork/002/task_003.java
+++ HomeWork/002/task_003.java
@@ -1,1 +1,51 @@
+import java.util.logging.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class task_003 {
+    /**
+     * @param args
+     * @throws IOException
+     * @throws SecurityException
+     */
+    public static void main(String[] args) throws SecurityException, IOException {
+        Logger logger = Logger.getLogger(task_003.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
+        double ans;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("\nВведите оператор (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+        System.out.print("Введите число: ");
+        double num2 = scanner.nextDouble();
+        
+        switch (op) {
+            case '+':
+                ans = num1 + num2;
+                break;
+            case '-':
+                ans = num1 - num2;
+                break;
+            case '*':
+                ans = num1 * num2;
+                break;
+            case '/':
+                ans = num1 / num2;
+                break;
+            default:
+                System.out.printf("Введите корректный оператор");
+                return;
+        }
+        System.out.print("\nРезультат:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+        logger.info(num1 + " " + op + " " + num2 + " = " + ans);
+        scanner.close();
+    }
+}
 
